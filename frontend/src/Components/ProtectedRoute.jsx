cfec90475db5151044b9abfc5e38c79346253f93
Mubla97 <munizblancop@gmail.com>
2024-06-06 23:45:14 +0200

    fix problem with accesstoken

--- frontend/src/Components/ProtectedRoute.jsx
+++ frontend/src/Components/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = ({ redirectPath = '/login' }) => {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem('accessToken');
   
   if (!token) {
     return <Navigate to={redirectPath} />;
