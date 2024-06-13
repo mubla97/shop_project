8f42cf572f86398476147e22f200d5bffa9009a8
Mubla97 <munizblancop@gmail.com>
2024-06-13 20:02:02 +0200

    add roles in backend + add role in sedders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Product;
+use App\Models\Role;
 use App\Models\Shop;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -17,16 +18,36 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        //CREATE ROLES DEFAULT
+        Role::create(['name' => 'user']);
+        Role::create(['name' => 'admin']);
 
         //  USER SEEDERS
-        User::factory(10)->create();
+        $users = User::factory(10)->create();
 
-        User::factory()->create([
+        $userRole = Role::where('name', 'user')->first();
+
+        foreach ($users as $user) {
+            if ($userRole) {
+                $user->roles()->attach($userRole->id);
+            }
+        }
+
+        // Create admin user and asign role
+        $admin = User::factory()->create([
             'username' => 'administrador',
-             'name' => 'Admin',
-             'email' => 'admin@hotmail.com',
-             'password' => Hash::make(env('ADMIN_USER_PASS')),
-         ]);
+            'name' => 'Admin',
+            'email' => 'admin@hotmail.com',
+            'password' => Hash::make(env('ADMIN_USER_PASS')),
+        ]);
+
+        // Assign 'admin' role to admin user
+        $adminRole = Role::where('name', 'admin')->first();
+
+        if ($adminRole) {
+            $admin->roles()->attach($adminRole->id);
+        }
+
 
           // Create 10 users, each with a store and 10 products.
         User::factory(10)->create()->each(function ($user) {
@@ -35,6 +56,13 @@ class DatabaseSeeder extends Seeder
 
             // Create 10 products for each store
             Product::factory(10)->create(['shop_id' => $shop->id]);
+
+            
+            $role = Role::where('name', 'user')->first();
+
+            if ($role) {
+                $user->roles()->attach($role->id);
+            }
         });
 
          
