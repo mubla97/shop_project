8041a5a5e55f145253d91c7749f0a4c971eec7a3
Mubla97 <munizblancop@gmail.com>
2024-06-14 22:53:35 +0200

    fix problem with roles

--- frontend/src/Services/Api.js
+++ frontend/src/Services/Api.js
@@ -4,10 +4,13 @@ import axios from 'axios';
 export const getUserRoles = async () => {
   try {
     const response = await axios.get("http://localhost:8080/user/roles", { withCredentials: true });
-
     return response.data;
   } catch (error) {
-    console.error('Error al obtener los roles del usuario:', error);
+    if (error.response && error.response.status === 401) {
+      console.log('Usuario no autenticado.');
+    } else {
+      console.error('Error al obtener los roles del usuario:', error);
+    }
     return [];
   }
 };
