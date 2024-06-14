3df95d8fca8127dafdcd20516b0b7a8b5c8ae6b6
Mubla97 <munizblancop@gmail.com>
2024-06-14 22:16:38 +0200

    fix backend

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
+    public function index(){
+        return view('welcome');
+    }
+
     public function login(Request $request)
     {
         $request->validate([
