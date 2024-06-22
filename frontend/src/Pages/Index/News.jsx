de452e4d328ac1dc7ca0aa439e1b1cde0e4f3e29
Mubla97 <munizblancop@gmail.com>
2024-06-22 20:47:32 +0200

    add last shops with image

--- frontend/src/Pages/Index/News.jsx
+++ frontend/src/Pages/Index/News.jsx
@@ -1,8 +1,13 @@
-import { Card, Button } from "react-bootstrap";
+import * as React from 'react';
 import { useNavigate } from "react-router-dom";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const News = ({ news }) => {
-
   const navigate = useNavigate();
 
   const redirectToViewShop = (id) => {
@@ -12,15 +17,41 @@ const News = ({ news }) => {
   return (
     <div className="container mt-4">
       <h2 className="mb-4">Last shops</h2>
-      <div className="d-flex overflow-auto">
+      <div
+        className="d-flex overflow-auto"
+        style={{ height: '420px', overflowY: 'hidden', paddingBottom: '4px' }}
+      >
         {news.map((n) => (
-          <div key={n.id} className="me-3">
-            <Card style={{ width: "400px" }} className="shadow-sm mb-3">
-              <Card.Body>
-                <Card.Title className="fw-bold">{n.name}</Card.Title>
-                <Card.Text>{n.community}</Card.Text>
-                <Button variant="success" onClick={() => redirectToViewShop(n.id)}>View more</Button>
-              </Card.Body>
+          <div key={n.id} className="me-3" style={{ marginBottom: '4px' }}>
+            <Card
+              sx={{
+                width: 300,
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                border: '2px solid #000',
+                borderRadius: '10px',
+                boxShadow: '0px 8px 16px rgba(0, 0, 0, 0.25)',
+              }}
+              className="shadow-sm"
+            >
+              <CardMedia
+                component="img"
+                sx={{ height: 200, objectFit: 'cover' }}
+                image={n.image ? `http://localhost:8080/images/shop_images/${n.image}` : 'http://localhost:8080/defaults/shop.jpg'}
+                title="Shop Image"
+              />
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography gutterBottom variant="h5" component="div" className="fw-bold">
+                  {n.name}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {n.community}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button variant="success" size="small" onClick={() => redirectToViewShop(n.id)}>View more</Button>
+              </CardActions>
             </Card>
           </div>
         ))}
