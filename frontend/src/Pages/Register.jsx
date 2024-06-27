ba831eb0300003ea7783c4db95575dc7bc308086
Mubla97 <munizblancop@gmail.com>
2024-06-27 20:01:49 +0200

    add new register

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,6 +1,17 @@
-import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBCheckbox,
+  MDBIcon
+}
+from 'mdb-react-ui-kit';
 
 const Register = () => {
 
@@ -72,108 +83,65 @@ const Register = () => {
   }
 
   return (
-    <>
-      <div className="container h-100 mb-3">
-        <hr />
-        <div className="row d-flex align-items-center justify-content-center h-100">
-          <div className="col-lg-12 col-xl-11">
-            <div className="card text-black" >
-              <div className="card-body p-md-5">
-                <div className="row justify-content-center">
-                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+    <MDBContainer fluid className='p-4 background-radial-gradient overflow-hidden'>
 
-                    <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Register</p>
-                    <form className="mx-1 mx-md-4" onSubmit={e => { doRegister(e) }}>
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
+    <MDBRow>
 
-                          <label className="form-label" >Username</label>
-                          <input type="text" className="form-control" id="username" placeholder="Introduce your username..." value={username}
-                            onChange={e => setUsername(e.target.value)} required />
-                        </div>
-                      </div>
+      <MDBCol md='6' className='text-center text-md-start d-flex flex-column justify-content-center'>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
+        <h1 className="my-5 display-3 fw-bold ls-tight px-3" style={{color: 'hsl(218, 81%, 95%)'}}>
+          The best offer <br />
+          <span style={{color: 'hsl(218, 81%, 75%)'}}>for your business</span>
+        </h1>
 
-                          <label className="form-label" >Email</label>
-                          <input type="email" className="form-control" id="email" placeholder="Introduce your email..." value={email}
-                            onChange={e => setEmail(e.target.value)} required />
-                        </div>
-                      </div>
+        <p className='px-3' style={{color: 'hsl(218, 81%, 85%)'}}>
+        Create your online business and sell your products in the simplest way!
+        </p>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
+      </MDBCol>
 
-                          <label className="form-label" >Name</label>
-                          <input type="text" className="form-control" id="name" placeholder="Introduce your name..." value={name}
-                            onChange={e => setName(e.target.value)} required />
-                        </div>
-                      </div>
+      <MDBCol md='6' className='position-relative'>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
+        <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+        <div id="radius-shape-2" className="position-absolute shadow-5-strong"></div>
 
-                          <label className="form-label" >Last name</label>
-                          <input type="text" className="form-control" id="lastname" placeholder="Introduce your firts last name..." value={lastname}
-                            onChange={e => setLastname(e.target.value)} required />
-                        </div>
-                      </div>
+        <MDBCard className='my-5 bg-glass'>
+          <MDBCardBody className='p-5'>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
+            <MDBRow>
+              <MDBCol col='6'>
+                <MDBInput wrapperClass='mb-4' label='First name' id='name' type='text' placeholder="Your first name..."/>
+              </MDBCol>
 
-                          <label className="form-label" >Phone</label>
-                          <input type="number" className="form-control" id="phone" placeholder="Introduce your phone..." value={phone}
-                            onChange={e => setPhone(e.target.value)} required />
-                        </div>
-                      </div>
+              <MDBCol col='6'>
+                <MDBInput wrapperClass='mb-4' label='Last name' id='lastname' type='text' placeholder="Your last name..."/>
+              </MDBCol>
+            </MDBRow>
+            <MDBRow>
+              <MDBCol col='6'>
+                <MDBInput wrapperClass='mb-4' label='Username' id='username' type='text' placeholder="Your username..."/>
+              </MDBCol>
 
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
+              <MDBCol col='6'>
+                <MDBInput wrapperClass='mb-4' label='Phone' id='phone' type='number' placeholder="Your phone..."/>
+              </MDBCol>
+            </MDBRow>
 
-                          <label className="form-label">Password</label>
-                          <input type="password" className="form-control" id="password" placeholder="Introduce your password..." value={password}
-                            onChange={e => setPassword(e.target.value)} required />
-                        </div>
-                      </div>
-                      <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
+            <MDBInput wrapperClass='mb-4' label='Email' id='email' type='email' placeholder="Your email..."/>
+            <MDBInput wrapperClass='mb-4' label='Password' id='password' type='password' placeholder="Your password..."/>
+            <MDBInput wrapperClass='mb-4' label='Repite Password' id='password_confirmation' type='password' placeholder="Repite your password..."/>
 
-                          <label className="form-label">Password confirmation</label>
-                          <input type="password" className="form-control" id="password_confirmation" placeholder="Repite your password..." value={password_confirmation}
-                            onChange={e => setPassword_confirmation(e.target.value)} required />
-                        </div>
-                      </div>
-                      <div className="d-flex justify-content-center mx-4 ">
-                        <button type="submit" id="signup" className="btn btn-primary btn-lg">Register</button>
+            <MDBBtn className='w-100 mb-4' size='md'>sign up</MDBBtn>
 
-                        <div className="d-flex justify-content-center mx-4">
-                          <Link to="/"><button type="submit" className="btn btn-warning btn-lg">Go Back</button></Link>
-                        </div>
-                      </div>
-                    </form>
+          </MDBCardBody>
+        </MDBCard>
 
-                  </div>
-                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+      </MDBCol>
 
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" className="img-fluid" alt="Sample img" />
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
-};
+    </MDBRow>
+
+  </MDBContainer>
+);
+}
 
 export default Register;
