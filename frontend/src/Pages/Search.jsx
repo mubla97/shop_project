8653e3891ffba788f52b4256199f352de5007d35
Mubla97 <munizblancop@gmail.com>
2025-03-25 21:15:59 +0100

    add pagination in serch

--- frontend/src/Pages/Search.jsx
+++ frontend/src/Pages/Search.jsx
@@ -3,13 +3,12 @@ import axios from 'axios';
 import { Card, Button, Form, Spinner } from 'react-bootstrap';
 import { useNavigate } from "react-router-dom";
 import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination'; // Importamos el componente de paginación de Material UI
 import {
   MDBContainer,
   MDBRow,
   MDBCol,
-} 
-from 'mdb-react-ui-kit';
-
+} from 'mdb-react-ui-kit';
 
 const Search = () => {
   const [query, setQuery] = useState('');
@@ -19,6 +18,8 @@ const Search = () => {
   const [filteredProducts, setFilteredProducts] = useState([]);
   const [searchType, setSearchType] = useState('shops');
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1); // Página actual
+  const [itemsPerPage, setItemsPerPage] = useState(12) // Resultados por página (ajustable)
   const navigate = useNavigate();
 
   const redirectToViewShop = (id) => {
@@ -57,103 +58,121 @@ const Search = () => {
     }
   }, [query, searchType, shops, products]);
 
+  // Paginación: Calcular los elementos a mostrar en la página actual
+  const paginate = (array) => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    return array.slice(startIndex, endIndex);
+  };
+
+  // Cambiar página cuando el usuario interactúa con el control de paginación
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
   return (
     <MDBContainer fluid className='background-radial-gradient overflow-hidden container-full-height'>
       <MDBRow>
-        
         <div className="mt-4">
           {isLoading ? (
             <div className="text-center">
               <Spinner animation="border" role="status" variant="light">
-                <span className="visually-hidden" >Loading...</span>
+                <span className="visually-hidden">Loading...</span>
               </Spinner>
             </div>
           ) : (
             <>
-                <Form>
-                  <Form.Group controlId="searchType" className="mb-3" style={{ position: 'relative', zIndex: 10 }}>
-                    <Typography variant="h4" className="m-4" style={{ color: 'white' }}>Select Type</Typography>
-                    <Form.Control
-                      as="select"
-                      value={searchType}
-                      onChange={(e) => setSearchType(e.target.value)}
-                    >
-                      <option value="shops">Shops</option>
-                      <option value="products">Products</option>
-                    </Form.Control>
-                  </Form.Group>
+              <Form>
+                <Form.Group controlId="searchType" className="mb-3" style={{ position: 'relative', zIndex: 10 }}>
+                  <Typography variant="h4" className="m-4" style={{ color: 'white' }}>Select Type</Typography>
+                  <Form.Control
+                    as="select"
+                    value={searchType}
+                    onChange={(e) => setSearchType(e.target.value)}
+                  >
+                    <option value="shops">Shops</option>
+                    <option value="products">Products</option>
+                  </Form.Control>
+                </Form.Group>
 
-                  <Form.Group controlId="search" style={{ position: 'relative', zIndex: 10 }}>
-                    <Form.Control
-                      type="text"
-                      placeholder={`Search for ${searchType}...`}
-                      value={query}
-                      onChange={(e) => setQuery(e.target.value)}
-                      disabled={isLoading}
-                    />
-                  </Form.Group>
-                </Form>
-                <MDBCol className='position-relative'>
-                
+                <Form.Group controlId="search" style={{ position: 'relative', zIndex: 10 }}>
+                  <Form.Control
+                    type="text"
+                    placeholder={`Search for ${searchType}...`}
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                    disabled={isLoading}
+                  />
+                </Form.Group>
+              </Form>
 
-                  {searchType === 'shops' && (
-                <>
-                
-                  <Typography variant="h4" className="m-4" style={{ color: 'white' }}>Shops</Typography>
-                  <div className="d-flex flex-wrap justify-content-center">
-                  <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
-                  <div id="radius-shape-2" className="position-absolute rounded-circle shadow-5-strong"></div>
-                    {filteredShops.map((shop) => (
-                      <Card key={shop.id} style={{ width: "400px" }} className="shadow-sm m-3">
-                        <Card.Body>
-                          <Card.Title className="fw-bold">{shop.name}</Card.Title>
-                          <Card.Text>{shop.community}</Card.Text>
-                          <Button
-                            className="w-100 mb-4 d-flex justify-content-center align-items-center"
-                            style={{
-                              height: '50px',
-                              padding: '0.5rem',
-                              position: 'relative',
-                              backgroundColor: '#fff', // blanco personalizado
-                              borderColor: '#fff', // Asegura que el borde sea del mismo color
-                              color: '#000', // Color del texto negro
-                              borderRadius: '0.25rem', // Opcional: bordes redondeados
-                              fontWeight: 'bold'
-                            }}
-                            onClick={() => redirectToViewShop(shop.id)}
-                          >
-                          <div className="w-100 d-flex justify-content-center align-items-center ">View more</div>
-                         </Button>
-                        </Card.Body>
-                      </Card>
-                    ))}
-                  </div>
-                </>
-              )}
+              <MDBCol className='position-relative'>
+                {searchType === 'shops' && (
+                  <>
+                    <Typography variant="h4" className="m-4" style={{ color: 'white' }}>Shops</Typography>
+                    <div className="d-flex flex-wrap justify-content-center">
+                      <div id="radius-shape-1" className="position-absolute rounded-circle shadow-5-strong"></div>
+                      <div id="radius-shape-2" className="position-absolute rounded-circle shadow-5-strong"></div>
+                      {paginate(filteredShops).map((shop) => (
+                        <Card key={shop.id} style={{ width: "400px" }} className="shadow-sm m-3">
+                          <Card.Body>
+                            <Card.Title className="fw-bold">{shop.name}</Card.Title>
+                            <Card.Text>{shop.community}</Card.Text>
+                            <Button
+                              className="w-100 mb-4 d-flex justify-content-center align-items-center"
+                              style={{
+                                height: '50px',
+                                padding: '0.5rem',
+                                position: 'relative',
+                                backgroundColor: '#fff', // blanco personalizado
+                                borderColor: '#fff', // Asegura que el borde sea del mismo color
+                                color: '#000', // Color del texto negro
+                                borderRadius: '0.25rem', // Opcional: bordes redondeados
+                                fontWeight: 'bold'
+                              }}
+                              onClick={() => redirectToViewShop(shop.id)}
+                            >
+                              <div className="w-100 d-flex justify-content-center align-items-center">View more</div>
+                            </Button>
+                          </Card.Body>
+                        </Card>
+                      ))}
+                    </div>
+                  </>
+                )}
 
-              {searchType === 'products' && (
-                <>
-                  <Typography variant="h4" className="m-4" style={{ color: 'white' }}>Products</Typography>
-                  <div className="d-flex flex-wrap justify-content-center">
-                    {filteredProducts.map((product) => (
-                      <Card key={product.id} style={{ width: "400px", margin: "10px" }} className="shadow-sm">
-                        <Card.Body>
-                          <Card.Title>{product.name}</Card.Title>
-                          <Card.Text>{product.description}</Card.Text>
-                        </Card.Body>
-                        <div className="card-footer d-flex justify-content-center">
-                          <Button variant="primary" onClick={() => redirectToViewShop(product.shop_id)}>View shop</Button>
-                        </div>
-                      </Card>
-                    ))}
-                  </div>
-                </>
-              )}
-                      </MDBCol>
+                {searchType === 'products' && (
+                  <>
+                    <Typography variant="h4" className="m-4" style={{ color: 'white' }}>Products</Typography>
+                    <div className="d-flex flex-wrap justify-content-center">
+                      {paginate(filteredProducts).map((product) => (
+                        <Card key={product.id} style={{ width: "400px", margin: "10px" }} className="shadow-sm">
+                          <Card.Body>
+                            <Card.Title>{product.name}</Card.Title>
+                            <Card.Text>{product.description}</Card.Text>
+                          </Card.Body>
+                          <div className="card-footer d-flex justify-content-center">
+                            <Button variant="primary" onClick={() => redirectToViewShop(product.shop_id)}>View shop</Button>
+                          </div>
+                        </Card>
+                      ))}
+                    </div>
+                  </>
+                )}
+
+                {/* Paginación */}
+                <div className="d-flex justify-content-center mt-4">
+                  <Pagination
+                    count={Math.ceil(filteredShops.length / itemsPerPage)} // Calcula la cantidad total de páginas
+                    page={currentPage}
+                    onChange={handlePageChange}
+                    color="primary"
+                  />
+                </div>
+              </MDBCol>
             </>
           )}
         </div>
-
       </MDBRow>
     </MDBContainer>
   );
